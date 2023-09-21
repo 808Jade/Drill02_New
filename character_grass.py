5b1755ce109962fd29218f623b875fd7eeb1a7af
2dgp (class) <129085079+808Jade@users.noreply.github.com>
2023-09-21 11:53:47 +0900

    run test

--- character_grass.py
+++ character_grass.py
@@ -1,63 +1,25 @@
 from pico2d import *
-import math
 
 open_canvas()
 
-grass = load_image('grass.png')
 character = load_image('character.png')
+grass = load_image('grass.png')
 
-x = 400
-y = 90
+clear_canvas_now()
+grass.draw_now(400,30)
+character.draw_now(400,90)
+delay(1)
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
 
 while True:
-    while (x < 750):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        x = x + 2
-        delay(0.01)
-
-    while (y < 550):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        y = y + 2
-        delay(0.01)
-
-    while (x > 50):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        x = x - 2
-        delay(0.01)
-
-    while (y > 90):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        y = y - 2
-        delay(0.01)
-
-    while (x < 400):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        x = x + 2
-        delay(0.01)
-
-    angle = -90
-    radius = 200
-
-    while angle <= 280:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-
-        x = 400 + radius * math.cos(math.radians(angle))
-        y = 290 + radius * math.sin(math.radians(angle))
-
-        character.draw_now(x, y)
-        angle += 1
-        delay(0.01)      
-
-
-close_canvas()
+    run_circle()
+    run_rectangle()
+    
